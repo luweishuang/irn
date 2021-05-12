1c778d9c797d871cf27e657480aae789ec8217bc
luweishuang <pifuchan@gmail.com>
2021-05-12 15:49:38 +0800

    debug cam train

--- run_sample.py
+++ run_sample.py
@@ -9,7 +9,7 @@ if __name__ == '__main__':
 
     # Environment
     parser.add_argument("--num_workers", default=os.cpu_count()//2, type=int)
-    parser.add_argument("--voc12_root", required=True, type=str,
+    parser.add_argument("--voc12_root", default='voc12/data/VOC12/train', type=str,
                         help="Path to VOC 2012 Devkit, must contain ./JPEGImages as subdirectory.")
 
     # Dataset
@@ -23,7 +23,7 @@ if __name__ == '__main__':
     # Class Activation Map
     parser.add_argument("--cam_network", default="net.resnet50_cam", type=str)
     parser.add_argument("--cam_crop_size", default=512, type=int)
-    parser.add_argument("--cam_batch_size", default=16, type=int)
+    parser.add_argument("--cam_batch_size", default=64, type=int)
     parser.add_argument("--cam_num_epoches", default=5, type=int)
     parser.add_argument("--cam_learning_rate", default=0.1, type=float)
     parser.add_argument("--cam_weight_decay", default=1e-4, type=float)
--- step/train_cam.py
+++ step/train_cam.py
@@ -13,34 +13,23 @@ from misc import pyutils, torchutils
 
 def validate(model, data_loader):
     print('validating ... ', flush=True, end='')
-
     val_loss_meter = pyutils.AverageMeter('loss1', 'loss2')
-
     model.eval()
 
     with torch.no_grad():
         for pack in data_loader:
             img = pack['img']
-
             label = pack['label'].cuda(non_blocking=True)
-
             x = model(img)
             loss1 = F.multilabel_soft_margin_loss(x, label)
-
             val_loss_meter.add({'loss1': loss1.item()})
-
     model.train()
-
     print('loss: %.4f' % (val_loss_meter.pop('loss1')))
-
     return
 
 
 def run(args):
-
     model = getattr(importlib.import_module(args.cam_network), 'Net')()
-
-
     train_dataset = voc12.dataloader.VOC12ClassificationDataset(args.train_list, voc12_root=args.voc12_root,
                                                                 resize_long=(320, 640), hor_flip=True,
                                                                 crop_size=512, crop_method="random")
@@ -67,26 +56,19 @@ def run(args):
     timer = pyutils.Timer()
 
     for ep in range(args.cam_num_epoches):
-
         print('Epoch %d/%d' % (ep+1, args.cam_num_epoches))
-
         for step, pack in enumerate(train_data_loader):
-
             img = pack['img']
             label = pack['label'].cuda(non_blocking=True)
-
             x = model(img)
             loss = F.multilabel_soft_margin_loss(x, label)
-
             avg_meter.add({'loss1': loss.item()})
-
             optimizer.zero_grad()
             loss.backward()
             optimizer.step()
 
             if (optimizer.global_step-1)%100 == 0:
                 timer.update_progress(optimizer.global_step / max_step)
-
                 print('step:%5d/%5d' % (optimizer.global_step - 1, max_step),
                       'loss:%.4f' % (avg_meter.pop('loss1')),
                       'imps:%.1f' % ((step + 1) * args.cam_batch_size / timer.get_stage_elapsed()),
